68b2ec30e05d382bfd2f76bd76f3e70244e9bb46
Rafa <rafa@Rafas-MacBook-Pro.local>
2021-12-28 11:15:52 -0800

    Display Value 0 in the initialization

--- index.js
+++ index.js
@@ -1,65 +1,39 @@
 class Calculator {
-  constructor(previousOperand, currentOperand) {
-    this.previousOperand = currentOperand
-    this.currentOperand = currentOperand
-    this.clear
+  constructor() {
+    this.value = 0
+    this.input = ''
+    this.displayInput = 0
   }
 
   appendNumber(value) {
-    this.currentOperand = value
-    console.log(value)
+    this.input = this.input + value
+    const currentInput = document.getElementById('input')
+    currentInput.innerHTML = this.input
+  }
+  clear() {
+    this.input = ''
+    this.value = ''
+    this.operation = undefined
   }
 }
-
-function clear() {
-  this.currentOperand = ''
-  this.previousOperand = ''
-  this.operation = undefined
-}
-
-function deleteNumber() {}
-
-function chooseOperation(operation) {}
-
-function compute() {}
-
-function updateDisplay() {
-  this.currentOperand.innerText = this.currentOperand
-}
-
 function getNumbers() {
-  return document.querySelectorAll('data-number')
+  return document.querySelectorAll('.data-number')
 }
 
-function operationButtons() {
-  return document.querySelector('[data-operation]').innerText
+function updateNumber() {
+  this.input = this.displayInput
+  const displayInput = document.getElementById('input')
+  displayInput.innerHTML = this.displayInput
 }
-function equalsButton() {
-  return document.querySelector('[data-equals]').innerText
-}
-function deleteButton() {
-  return document.querySelector('[data-delete]').innerText
-}
-function allClearButtons() {
-  return document.querySelector('[data-all-clear]').innerText
-}
-function previousOperand() {
-  return document.querySelector('[data-previous-operand]').innerText
-}
-function currentOperand() {
-  return document.querySelector('[data-current-operand]').innerText
-}
-
-// function calculator() {
-//   return new calculator(previousOperand, currentOperand)
-// }
 
 function bindClickEvents() {
+  const _calculator = new Calculator()
   getNumbers().forEach((button) => {
     button.addEventListener('click', () => {
-      const _calculator = new Calculator()
       _calculator.appendNumber(button.innerText)
-      _calculator.updateDisplay()
     })
   })
 }
+
+updateNumber()
+bindClickEvents()
